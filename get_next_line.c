eb19aa24658e117cfc59377a9127f12e5f23851e
RPanetta <panetta.rocio@gmail.com>
2025-11-09 23:25:20 +0100

    get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,22 @@
 /*   By: rpanetta <rpanetta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 11:45:02 by rpanetta          #+#    #+#             */
-/*   Updated: 2025/11/06 11:51:46 by rpanetta         ###   ########.fr       */
+/*   Updated: 2025/11/09 23:24:14 by rpanetta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *get_next_line(int fd);
+char *get_next_line(int fd)
+{
+	
+}
+int	main(void)
+{
+	int fd;
+
+	fd = open("text.txt", O_RDWR);
+	printf("%s\n", get_next_line(fd));
+	printf("%s\n", get_next_line(fd));
+	printf("%s\n", get_next_line(fd));
+	printf("%s\n", get_next_line(fd));
+	return (0);
+}
